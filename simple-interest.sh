87261fe6923817aac3327aff50633093dfc78556
AansRehman <106799284+AansRehman@users.noreply.github.com>
2023-12-24 15:47:48 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,17 @@
 
+#!/bin/bash
+
+# Simple script to run the HelloWorld Python program
+
+# Check if Python is installed
+if command -v python3 &>/dev/null; then
+    echo "Python found. Running HelloWorld program..."
+else
+    echo "Error: Python not found. Please install Python to run this program."
+    exit 1
+fi
+
+# Run the HelloWorld Python program
+python3 helloworld.py
+
+chmod +x simple-file.sh
